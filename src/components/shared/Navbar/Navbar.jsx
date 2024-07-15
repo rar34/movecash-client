04e7a1b85a006af74957a78ce35730df1be85741
rar34 <ra.cse34@gmail.com>
2024-07-16 00:34:02 +0600

    add: firebase config file

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,48 +1,75 @@
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    const user = false;
+    const user = true;
+    const isAdmin = true
     const navLinks = <>
-        <li><NavLink  to="/"  style={({ isActive }) => { return isActive ? { backgroundColor: '#FFFFFF', color:"black" } : {}; }}>Home</NavLink></li>
-        <li><NavLink to="/cashout"  style={({ isActive }) => { return isActive ? { backgroundColor: '#F7931D' } : {}; }}>Send Money</NavLink></li>
+        <li><NavLink to="/" style={({ isActive }) => { return isActive ? { backgroundColor: '#FFFFFF', color: "black" } : {}; }}>Home</NavLink></li>
+        <li><NavLink to="/cashout" style={({ isActive }) => { return isActive ? { backgroundColor: '#F7931D' } : {}; }}>Send Money</NavLink></li>
     </>
+
+
+
+    const handleLogOut = () => {
+        // logOut()
+        //     .then(result => {
+        //         toast.success("log out successful")
+        //         console.log(result.user)
+        //     })
+        //     .catch(error => {
+        //         console.log(error)
+        //         toast.error(error.message)
+        //     })
+    }
+
     return (
         <div className="bg-tealColor">
-            <div className="navbar text-white container mx-auto">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
+            <div className=" bg-tealColor  sticky top-0 z-40">
+                <div className="drawer z-50 container mx-auto text-white">
+                    <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content flex flex-col">
+                        {/* Navbar */}
+                        <div className="w-full navbar">
+                            <div className="flex-none lg:hidden">
+                                <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                                </label>
+                            </div>
+                            <div className="flex-1 text-xl md:text-3xl font-bold"><Link to="/">MoveCash</Link></div>
+                            <div className="flex-none font-bold hidden lg:block">
+                                <ul className="menu text-xl menu-horizontal">
+                                    {navLinks}
+                                </ul>
+                            </div>
+
+                            {
+                                user ? <div className="dropdown dropdown-end " >
+                                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar tooltip">
+                                        <div className="w-10 border-2 border-white rounded-full ">
+                                            <img alt="Tailwind CSS Navbar component" src={user.image || "https://i.ibb.co/ZX6HMzF/pp.jpg"} />
+                                        </div>
+
+                                    </div>
+                                    <ul tabIndex={0} className="mt-3 z-[10] p-2 shadow menu menu-sm text-white font-bold dropdown-content bg-[#6F42C1] w-36">
+                                        <li><button className=" border" disabled>Balance: {user?.displayName}</button></li>
+
+                                        <li className="hover:bg-gray-500"><Link to={isAdmin ? '/dashboard/organizer-profile' : '/dashboard/analytics'}>Dashboard</Link></li>
+                                        <li className="hover:bg-gray-500 text-red-500"><button onClick={handleLogOut}>Logout</button></li>
+                                    </ul>
+                                </div>
+                                    :
+                                    <Link to="/login"><button className="btn btn-outline border hover:bg-[#F7931D] border-[#F5F5DC] text-white text-xl font-medium md:font-bold">Join US</button></Link>
+                            }
                         </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+
+                    </div>
+                    {/* for responsive device */}
+                    <div className="drawer-side">
+                        <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                        <ul className="menu font-bold text-2xl w-64 min-h-full bg-[#6F42C1]">
                             {navLinks}
-                            
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-2xl">MoveCash</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu text-lg menu-horizontal px-1">
-                        {navLinks}
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    {
-                        user ? <p>Logout</p> : <Link to="/login" className="btn btn-md font-semibold text-xl">Login</Link>
-                    }
                 </div>
             </div>
         </div>
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,86 @@
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import PropTypes from 'prop-types'
+import useAxiosPublic from "../hooks/useAxiosPublic";
+import app from "../firebase/firebase.config";
+
+
+export const AuthContext = createContext(null)
+
+const auth = getAuth(app)
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const axiosPublic = useAxiosPublic();
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+   
+
+    const logOut = () => {
+        setLoading(true)
+        signOut(auth);
+    }
+
+    const profileUpdate = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        })
+    }
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            console.log('Current user: ', currentUser)
+            if (currentUser) {
+                const userInfo = { email: currentUser.email }
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token)
+                            setLoading(false)
+                        }
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token')
+                setLoading(false)
+            }
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, [axiosPublic])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut,
+        profileUpdate
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
